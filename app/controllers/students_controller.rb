71cb47ff3c1de32de6b78abab9db9e29c82a7f0f
huazai <470021389@qq.com>
2017-03-28 21:22:59 +0800

    get all courses and teachers of students

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,8 @@
+class StudentsController < ApplicationController
+  def show
+    @student = Student.find(params[:id])
+
+    render json: @student.to_json(:include => {:courses => {:include => :teacher}})
+  end
+  
+end
